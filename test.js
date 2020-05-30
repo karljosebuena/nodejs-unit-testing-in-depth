2b327f8458f6be302b3cc9cdd1a490721d1fb4fb
Karl Beaumont <karl.beaumont@firstmac.com.au>
2020-05-30 08:04:26 +0800

    Introcution to Before & After

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 const assert = require('assert');
+const colors = require('colors');
 
 describe('file to be tested', () => {
     context('function to be tested', () => {
+        before(() => {
+            console.log('=====before'.green)
+        });
+        after(() => {
+            console.log('=====after'.green)
+        });
+        beforeEach(() => {
+            console.log('=====beforeEach'.yellow)
+        });
+        afterEach(() => {
+            console.log('=====afterEach'.yellow)
+        });
+
+
         it('should do something', () => {
             assert.equal(1, 1);
         });
