57c72588ad79f760a1087eadc8ecfaf7301501ad
karljosebuena <karljosebuena@gmail.com>
2020-05-30 12:12:33 +0800

    Testing standard function.

---
+++ lib-introduction/v1/nested/nested.test.js
@@ -0,0 +1,5 @@
+describe('nested test', () => {
+    context('a function to test in nested file', () => {
+        it('pending test from nested file')
+    });
+});
---
+++ test/v1/bar.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const colors = require('colors');
+
+describe('file to be tested', () => {
+    context('function to be tested', () => {
+        before(() => {
+            console.log('=====before'.green)
+        });
+        after(() => {
+            console.log('=====after'.green)
+        });
+        beforeEach(() => {
+            console.log('=====beforeEach'.yellow)
+        });
+        afterEach(() => {
+            console.log('=====afterEach'.yellow)
+        });
+        it('should do something', () => {
+            assert.equal(1, 1);
+        });
+        it('should do something else ', () => {
+            assert.deepEqual({name: 'karl'}, {name: 'karl'})
+        });
+        it('this is a pending test');
+    });
+
+    context('another function to be tested', () => {
+        it('another pending test');
+    });
+});
